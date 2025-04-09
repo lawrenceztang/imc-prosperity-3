9f18d86bdf99b623c3777c4be858433e753741cb
lawrencetang <lawrenceztang@gmail.com>
2025-04-08 18:10:00 -0700

    Add logger

--- lawrence/round1.py
+++ lawrence/round1.py
@@ -1,30 +1,29 @@
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
-import string
 
+from logger import Logger
+
+logger = Logger()
 class Trader:
-
     def run(self, state: TradingState):
-        product_function_map = {"RAINFOREST_RESIN": self.trade_resin}
+        product_function_map = {
+            "RAINFOREST_RESIN": self.trade_resin,
+            # "KELP": self.trade_kelp,
+            # "SQUID_INK": self.trade_ink
+        }
 
-        print("traderData: " + state.traderData)
-        print("Observations: " + str(state.observations))
-
-        # Orders to be placed on exchange matching engine
         result = {}
+        self.log_state(state)
         for product in state.order_depths:
             if product in product_function_map:
                 function = product_function_map[product]
                 orders = function(state.order_depths[product])
                 result[product] = orders
 
-        # String value holding Trader state data required.
-        # It will be delivered as TradingState.traderData on next execution.
-        traderData = "SAMPLE"
-
-        # Sample conversion request. Check more details below.
+        trader_data = "SAMPLE"
         conversions = 1
-        return result, conversions, traderData
+        logger.flush(state, result, conversions, trader_data)
+        return result, conversions, trader_data
 
     def trade_resin(self, order_depth):
         theoretical_price = 10000
@@ -32,19 +31,73 @@ class Trader:
 
         product = "RAINFOREST_RESIN"
         orders: List[Order] = []
-        print("Acceptable price : " + str(theoretical_price))
+        logger.print("Acceptable price : " + str(theoretical_price))
+
+        # orders.append(Order(product, 10000 - spread, 10))
+        # orders.append(Order(product, 10000 + spread, -10))
 
         if len(order_depth.sell_orders) != 0:
             best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-            print(f"Best market ask: {best_ask_amount}@{best_ask}")
             if int(best_ask) < theoretical_price - spread:
-                print("BUY", str(-best_ask_amount) + "x", best_ask)
+                logger.print("BUY", str(-best_ask_amount) + "x", best_ask)
                 orders.append(Order(product, best_ask, -best_ask_amount))
 
         if len(order_depth.buy_orders) != 0:
             best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-            print(f"Best market bid: {best_bid_amount}@{best_bid}")
             if int(best_bid) > theoretical_price + spread:
-                print("SELL", str(best_bid_amount) + "x", best_bid)
+                logger.print("SELL", str(best_bid_amount) + "x", best_bid)
                 orders.append(Order(product, best_bid, -best_bid_amount))
         return orders
+
+    def trade_kelp(self, order_depth):
+        theoretical_price = 10000
+        spread = 1
+
+        product = "KELP"
+        orders: List[Order] = []
+        logger.print("Acceptable price : " + str(theoretical_price))
+
+        if len(order_depth.sell_orders) != 0:
+            best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+            if int(best_ask) < theoretical_price - spread:
+                logger.print("BUY", str(-best_ask_amount) + "x", best_ask)
+                orders.append(Order(product, best_ask, -best_ask_amount))
+
+        if len(order_depth.buy_orders) != 0:
+            best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+            if int(best_bid) > theoretical_price + spread:
+                logger.print("SELL", str(best_bid_amount) + "x", best_bid)
+                orders.append(Order(product, best_bid, -best_bid_amount))
+        return orders
+
+    def trade_ink(self, order_depth):
+        theoretical_price = 10000
+        spread = 1
+
+        product = "SQUID_INK"
+        orders: List[Order] = []
+        logger.print("Acceptable price : " + str(theoretical_price))
+
+        if len(order_depth.sell_orders) != 0:
+            best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+            if int(best_ask) < theoretical_price - spread:
+                logger.print("BUY", str(-best_ask_amount) + "x", best_ask)
+                orders.append(Order(product, best_ask, -best_ask_amount))
+
+        if len(order_depth.buy_orders) != 0:
+            best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+            if int(best_bid) > theoretical_price + spread:
+                logger.print("SELL", str(best_bid_amount) + "x", best_bid)
+                orders.append(Order(product, best_bid, -best_bid_amount))
+        return orders
+
+    def log_state(self, state):
+        logger.print("traderData: " + state.traderData)
+        logger.print("Observations: " + str(state.observations))
+
+        for product in state.order_depths:
+            order_depth = state.order_depths[product]
+            best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+            best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+            logger.print(f"{product} best market bid: {best_bid_amount}@{best_bid}")
+            logger.print(f"{product} best market ask: {best_ask_amount}@{best_ask}")
